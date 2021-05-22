61fd1f7f0171f9285050113829ab5d5fdac831f7
Alexander Semenov <modern.rising@outlook.com>
2021-05-22 20:44:44 +0300

    dekstop version 1.0

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        speed: 800,
+        draggable: false,
+        rows: 2,
+        slidesPerRow:2,
+        /* adaptiveHeight:true, */
+    });
+  });
