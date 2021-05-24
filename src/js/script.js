3111b3d7db6587b73753770d38ad34babb5fc71d
Alexander Semenov <modern.rising@outlook.com>
2021-05-24 16:49:50 +0300

    dekstop + mobile 1.0

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,23 @@ $(document).ready(function(){
         draggable: false,
         rows: 2,
         slidesPerRow:2,
-        /* adaptiveHeight:true, */
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                arrows:false,
+              }
+            }
+        ]
+            
     });
   });
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".banner__menu");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("banner__menu_active");
+});
+
